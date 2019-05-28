8e6260f4de82f065a4cb099e2fa86bbef23d1f48
Moises Fuentes Dimas <dfuentes@univim.edu.mx>
2019-05-28 00:05:13 -0500

    Se creo la configuracion de la base de datos

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,1 +1,15 @@
- 
+<?php
+    //Configuracion de acceso a la base de datos
+    define('DB_HOST','localhost');
+    define('DB_USUARIO','admin');
+    define('DB_PASSWORD','Sup3rAdm1n.');
+    define('DB_NOMBRE','blog1');
+
+    //Ruta de la aplicacion
+    define('RUTA_APP', dirname(dirname(__FILE__)));
+
+    //Ruta url Ejemplo: http://localhost/mvc2
+    define('RUTA_URL','_URL_');
+
+    define('NOMBRESITIO', '_NOMBRE_SITIO');
+
