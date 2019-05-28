366bb52d0e9041c1e69f72f80571ffdec607a829
Moises Fuentes Dimas <dfuentes@univim.edu.mx>
2019-05-28 00:12:16 -0500

    Se muestra codigo en forma de lista

--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -1,1 +1,8 @@
- 
+<?php require RUTA_APP . '/vistas/inc/header.php'; ?>
+<p><?php echo $datos['titulo']; ?></p>
+<ul>
+    <?php foreach($datos['articulos'] as $articulo):  ?>
+        <li><?php echo $articulo->titulo; ?></li>
+    <?php endforeach;  ?>
+</ul>
+<?php require RUTA_APP . '/vistas/inc/footer.php'; ?>
